2457ffd8e1cc4e785799553f49d3ff514fe3232c
Sylvain Mazaleyrat <sylvain.mazaleyrat@gmail.com>
2018-08-17 20:34:45 +0200

    test

--- docker/vue-storefront-api/vue-storefront-api.sh
+++ docker/vue-storefront-api/vue-storefront-api.sh
@@ -5,7 +5,6 @@ yarn install || exit $?
 
 yarn restore
 yarn migrate
-git clone https://github.com/magento/magento2-sample-data.git var/magento2-sample-data
 
 if [ "$VS_ENV" = 'dev' ]; then
   yarn dev
